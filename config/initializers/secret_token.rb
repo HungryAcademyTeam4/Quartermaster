8947b9e38b183a8795992459bc418d2a88144446
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-12 14:13:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quartermaster::Application.config.secret_token = 'ed09774839e1e50257d41e5931bef3873172635fe4cfbf1aeb3b0c107ba8d46752d5e184da9455b886e9c7799e64bb1db0bd5c15bc7355b9ff144ebdab0f094f'
